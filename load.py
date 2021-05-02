1d8fb611c53615146fdb9905715439ba8ab89fdd
ktmurray1999 <43282256+ktmurray1999@users.noreply.github.com>
2021-05-02 18:34:38 -0400

    update

--- load.py
+++ load.py
@@ -9,16 +9,16 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
-data, sr = librosa.load('example.mp3')
-x = data[:10*sr]
-plt.plot(x)
+# data, sr = librosa.load('example.mp3')
+# x = data[:10*sr]
+# plt.plot(x)
 # np.save('example.npy', x)
 
-# y = np.load('example.npy')
-# A = librosa.stft(y)
-# S = np.real(A)
+y = np.load('example.npy')
+A = librosa.stft(y)
+S = np.imag(A)
 
-# fig, ax = plt.subplots()
-# img = librosa.display.specshow(librosa.amplitude_to_db(S,ref=np.max),y_axis='log', x_axis='time', ax=ax)
-# ax.set_title('Power spectrogram')
-# fig.colorbar(img, ax=ax, format="%+2.0f dB")
+fig, ax = plt.subplots()
+img = librosa.display.specshow(librosa.amplitude_to_db(S,ref=np.max),y_axis='log', x_axis='time', ax=ax)
+ax.set_title('Power spectrogram')
+fig.colorbar(img, ax=ax, format="%+2.0f dB")
