4f7bc35f952f591ccc4445aae611fb82ab7b631c
ktmurray1999 <43282256+ktmurray1999@users.noreply.github.com>
2021-05-04 10:13:44 -0400

    Added Convolutional Autoencoder
    In addition to a simple linear autoencoder, I created a convolutional autoencoder as well.

--- simple_model.py
+++ simple_model.py
@@ -15,9 +15,9 @@ import matplotlib.pyplot as plt
 device = torch.device("cuda:0")
 
 
-class SimpleAutoencoder(nn.Module):
+class LinearAutoencoder(nn.Module):
     def __init__(self):
-        super(SimpleAutoencoder, self).__init__()
+        super(LinearAutoencoder, self).__init__()
         self.encode1 = nn.Linear(220500, 300)
         self.encode2 = nn.Linear(300,50)
         self.decode2 = nn.Linear(50,300)
@@ -31,6 +31,37 @@ class SimpleAutoencoder(nn.Module):
         x = self.activation_func(self.decode2(x))
         x = self.decode3(x)
         return x
+
+
+class ConvolutionalAutoencoder(nn.Module):
+    def __init__(self):
+        super(ConvolutionalAutoencoder, self).__init__()
+        self.encode1 = nn.Conv1d(1, 4, 501, padding=255)
+        self.encode2 = nn.Conv1d(4, 8, 251, padding=125)
+        self.encode3 = nn.Conv1d(8, 16, 125, padding=62)
+        self.decode1 = nn.ConvTranspose1d(16, 8, 125, padding=62)
+        self.decode2 = nn.ConvTranspose1d(8, 4, 252, padding=125)
+        self.decode3 = nn.ConvTranspose1d(4, 1, 501, padding=255)
+        
+        self.pool = nn.MaxPool1d(10, stride=10, return_indices=True)
+        self.unpool = nn.MaxUnpool1d(10, stride=10)
+        self.activation_func = nn.Tanh()
+
+    def forward(self, x):
+        x = torch.unsqueeze(x, 1)
+        x = self.activation_func(self.encode1(x))
+        y = x.size()
+        print(y)
+        x, p1 = self.pool(x)
+        x = self.activation_func(self.encode2(x))
+        x, p2 = self.pool(x)
+        x = self.activation_func(self.encode3(x))
+        x = self.activation_func(self.decode1(x))
+        x = self.unpool(x, p2)
+        x = self.activation_func(self.decode2(x))
+        x = self.unpool(x, p1, output_size=y)
+        x = self.decode3(x)
+        return torch.squeeze(x)
 
 
 class Dataset(torch.utils.data.Dataset):
@@ -83,9 +114,16 @@ def OptimizeModel(net, dataset, label, epochs):
     
     
 if __name__ == "__main__":
-    net = SimpleAutoencoder().to(device)
-    OptimizeModel(net, 'clips', 444, 50)
-    data, sr = librosa.load('Q:\Documents\TDS SuperUROP\\music-vae\\clips\\lofi-track-1-clip-2.mp3')
-    output = net(torch.tensor(data).to(device)).cpu().detach().numpy()
-    sf.write('output_clip_2.wav',output,sr)
+    # net = LinearAutoencoder().to(device)
+    # OptimizeModel(net, 'clips', 444, 50)
+    # data, sr = librosa.load('Q:\Documents\TDS SuperUROP\\music-vae\\clips\\lofi-track-1-clip-2.mp3')
+    # output = net(torch.tensor(data).to(device)).cpu().detach().numpy()
+    # sf.write('output_clip_2.wav',output,sr)
+    # torch.save(net.state_dict(), 'linear_autoencoder.pt')
     
+    net = ConvolutionalAutoencoder().to(device)
+    OptimizeModel(net, 'clips', 444, 50)
+    data, sr = librosa.load('Q:\Documents\TDS SuperUROP\\music-vae\\clips\\lofi-track-1-clip-3.mp3')
+    output = net(torch.tensor(data).to(device)).cpu().detach().numpy()
+    sf.write('output_clip_3.wav',output,sr)
+    torch.save(net.state_dict(), 'convolutional_autoencoder.pt')
